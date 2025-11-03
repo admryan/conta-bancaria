cf22ed6176ce6154bcdc29b44b43d3087fa949d8
Ryan Alves <ryannalvess51@gmail.com>
2025-11-03 15:18:38 -0300

    Primeiro commit - Projeto Conta Bancária em C#

---
+++ Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    internal class BankAccount
+    {
+        public int AccountNumber { get; private set; }
+        public string Holder { get; set; }
+        public double Balance { get; private set; }
+
+        public BankAccount(int accountNumber, string holder)
+        {
+            AccountNumber = accountNumber;
+            Holder = holder;
+        }
+
+        public BankAccount(int accountNumber, string holder, double initialDeposit)
+            : this(accountNumber, holder)
+        {
+            Deposit(initialDeposit);
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            Balance -= amount + 5.00; // withdrawal fee
+        }
+
+        public override string ToString()
+        {
+            return $"Account {AccountNumber}, Holder: {Holder}, Balance: ${Balance.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+using Course;
+
+namespace BankApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria account;
+
+            Console.Write("Enter account number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter account holder: ");
+            string holder = Console.ReadLine();
+
+            Console.Write("Will there be an initial deposit (y/n)? ");
+            char answer = char.Parse(Console.ReadLine());
+
+            if (answer == 'y' || answer == 'Y')
+            {
+                Console.Write("Enter initial deposit value: ");
+                double initialDeposit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                account = new ContaBancaria(number, holder, initialDeposit);
+            }
+            else
+            {
+                account = new ContaBancaria(number, holder);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Account data:");
+            Console.WriteLine(account);
+
+            Console.WriteLine();
+            Console.Write("Enter a deposit amount: ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            account.Deposito(amount);
+
+            Console.WriteLine();
+            Console.WriteLine("Updated account data:");
+            Console.WriteLine(account);
+
+            Console.WriteLine();
+            Console.Write("Enter a withdraw amount: ");
+            double withdraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            account.Saque(withdraw);
+
+            Console.WriteLine();
+            Console.WriteLine("Final account data:");
+            Console.WriteLine(account);
+        }
+    }
+}
